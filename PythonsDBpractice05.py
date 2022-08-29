a1f22cba54179f6d8eb48420f26a41c7af139ae0
Doosan <doosan0425@gmail.com>
2022-08-29 09:49:41 +0900

    가버나움 영화의 평점을 0으로 만들때 0은 문자열로 넣기

--- PythonsDBpractice05.py
+++ PythonsDBpractice05.py
@@ -3,4 +3,4 @@ from pymongo import MongoClient
 client = MongoClient('mongodb+srv://test:sparta@cluster0.9cihgwo.mongodb.net/?retryWrites=true&w=majority')
 db = client.dbsparta
 
-db.movies.update_one({'title': '가버나움'}, {'$set': {'star': 0}})
+db.movies.update_one({'title': '가버나움'}, {'$set': {'star': '0'}})
