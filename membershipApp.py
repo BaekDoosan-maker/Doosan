3ed178890e9e454fa0b18828758dcb0a57629e2d
Doosan <doosan0425@gmail.com>
2022-08-30 12:46:11 +0900

    회원가입 python server, MongoDB Atlas connected

--- membershipApp.py
+++ membershipApp.py
@@ -1,35 +1,44 @@
 from flask import Flask, render_template, request, jsonify
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
+
 client = MongoClient('mongodb+srv://test:sparta@cluster0.9cihgwo.mongodb.net/Cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 @app.route("/ds", methods=["POST"])
 def web_ds_post():
     id_receive = request.form['id_give']
     password_receive = request.form['password_give']
     passwordCheck_receive = request.form['passwordCheck_give']
+    phone_receive = request.form['phone_give']
+    address_receive = request.form['address_give']
     gender_receive = request.form['gender_give']
 
-    doc= {
-        'id':id_receive,
-        'password':password_receive,
-        'passwordCheck':passwordCheck_receive,
-        'gender':gender_receive
+    doc = {
+        'id': id_receive,
+        'password': password_receive,
+        'passwordCheck': passwordCheck_receive,
+        'phone': phone_receive,
+        'address': address_receive,
+        'gender': gender_receive
     }
     db.ds.insert_one(doc)
 
     return jsonify({'msg': '회원가입 완료!'})
 
+
 @app.route("/ds", methods=["GET"])
 def web_mars_get():
     order_list = list(db.ds.find({}, {'_id': False}))
-    return jsonify({'orders':order_list})
+    return jsonify({'orders': order_list})
 
 
 if __name__ == '__main__':
